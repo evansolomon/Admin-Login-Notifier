78f35f22f17df914cb06b5c2186e9d194f78382c
evansolomon <evansolomon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-18 08:24:37 +0000

    Store login attempt data in an option and show it on an admin page under the Tools menu
    git-svn-id: http://plugins.svn.wordpress.org/admin-login-notifier/trunk@559753 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin-login-notifier.php
+++ admin-login-notifier.php
@@ -30,6 +30,11 @@ function admin_login_notifier( $null, $username, $password ) {
 		$message .= "They used the password: {$password}\n\n";
 		$message .= "Silly bots!";
 
+		$alerts = get_option( 'aln_login_attempts' );
+		$alerts = (array) $alerts;
+		$alerts[] = array( 'time' => time(), 'password' => $password );
+		update_option( 'aln_login_attempts', $alerts );
+
 		wp_mail( $email_address, $subject, $message );
 	}
 
@@ -37,3 +42,30 @@ function admin_login_notifier( $null, $username, $password ) {
 	return $null;
 }
 add_filter( 'authenticate', 'admin_login_notifier', 10, 3 );
+
+function aln_submenu() {
+	add_submenu_page( 'tools.php', 'Admin Login Notifier', 'Admin Login Notifier', 'manage_options', 'admin-login-notifier', 'aln_submenu_ui' );
+}
+add_action( 'admin_menu', 'aln_submenu' );
+
+function aln_submenu_ui() {
+	global $title;
+	if ( !current_user_can( 'manage_options' ) )
+		wp_die( 'You do not have sufficient permissions to access this page.' );
+
+	// Page header
+	echo sprintf( '<div id="vip-scanner" class="wrap">%s<h2>%s</h2></div>', get_screen_icon( 'tools' ), esc_html( $title ) );
+
+	$alerts = get_option( 'aln_login_attempts' );
+	if( !$alerts || !is_array( $alerts ) )
+		return;
+
+	echo '<table>';
+	echo '<tr><th>Date</th><th>Password</th></tr>';
+	foreach( $alerts as $alert ) {
+		if( !$alert )
+			continue;
+		echo sprintf( '<tr><td>%s</td><td>%s</td></tr>', date( 'M d, Y', $alert['time'] ), esc_html( $alert['password'] ) );
+	}
+	echo '</table>';
+}
