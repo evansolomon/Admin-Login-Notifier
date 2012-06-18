cfa4504dc8a7758f4809e5c099ae8dd50459395c
evansolomon <evansolomon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-18 08:36:56 +0000

    This seems like enough changes to bump the version number
    git-svn-id: http://plugins.svn.wordpress.org/admin-login-notifier/trunk@559795 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin-login-notifier.php
+++ admin-login-notifier.php
@@ -5,6 +5,7 @@ Description: Lots of bots (or just rude people) try to login to WordPress sites 
 Author: evansolomon
 Author URI: http://evansolomon.me
 License: GPLv2 or later
+Version: 2
 */
 
 class Admin_Login_Notifier {
